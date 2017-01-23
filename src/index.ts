e5223b0903eaebd2728bffb4424627a1173a320e
Tobias Walle <tobias.walle@gmail.com>
2017-01-23 19:12:45 +0100

    Add documentation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
 
+/**
+ * Wrap a component with the provided styles.
+ * @param styles The styles you want assign.
+ * @return {(target:any)=>void} A function that wraps the target with the provided styles styles .
+ */
 export function WithStyles(styles) {
     return function (target) {
         withStyles(styles)(target);
