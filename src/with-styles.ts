5f2021c9fed639e92031546d995bb595cc347a31
Tobias Walle <tobias.walle@gmail.com>
2017-04-14 14:29:47 +0200

    Prevent default lifecylce methods in WithStyles

--- src/with-styles.ts
+++ src/with-styles.ts
@@ -6,24 +6,30 @@ import * as React from 'react';
  * @return {(target:any)=>void} A function that wraps the target with the provided styles.
  */
 export function WithStyles(styles: any) {
-    return function (Component): void {
+  return function (Component): void {
 
-        // Add context types
-        Component.contextTypes = {
-            insertCss: React.PropTypes.func
-        };
+    // Add context types
+    const oldContextTypes = Component.contextTypes || {};
+    Component.contextTypes = {
+      ...oldContextTypes,
+      insertCss: React.PropTypes.func
+    };
 
 
-        // Subscribe to lifecycle methods
-        Component.prototype.componentWillMount = function () {
-            if (this.context.insertCss) {
-                this.removeCss = this.context.insertCss(styles);
-            }
-        };
+    // Subscribe to lifecycle methods
+    const oldComponentWillMount = Component.prototype.componentWillMount;
+    Component.prototype.componentWillMount = function () {
+      if (this.context.insertCss) {
+        this.removeCss = this.context.insertCss(styles);
+      }
+      oldComponentWillMount && oldComponentWillMount();
+    };
 
-        Component.prototype.componentWillUnmount = function () {
-            setTimeout(this.removeCss, 0);
-        }
-
+    const oldComponentWillUnmount = Component.prototype.componentWillUnmount;
+    Component.prototype.componentWillUnmount = function () {
+      oldComponentWillUnmount && oldComponentWillUnmount();
+      setTimeout(this.removeCss, 0);
     }
+
+  }
 }
